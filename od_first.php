e89ae49e9ce1947575915554b499af1694a02296
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-09-06 11:24:36 +0200

    JS/CSS only executed on odfirst admin module

--- od_first.php
+++ od_first.php
@@ -53,8 +53,11 @@ class Od_first extends Module{
         && $this->unregisterHook('actionAdminControllerSetMedia');
     }
     public function hookActionAdminControllerSetMedia(){
-        $this->context->controller->addJs(_PS_MODULE_DIR_.'od_first/views/js/odjs.js');
-        $this->context->controller->addCSS(_PS_MODULE_DIR_.'od_first/views/css/styles.css');
+        if (get_class($this->context->controller) == 'AdminOdFirstController'){
+            $this->context->controller->addJs(_PS_MODULE_DIR_.'od_first/views/js/odjs.js');
+            $this->context->controller->addCSS(_PS_MODULE_DIR_.'od_first/views/css/styles.css');
+        }
+        
     }
     /**
      * Redirect to the admin controller link
